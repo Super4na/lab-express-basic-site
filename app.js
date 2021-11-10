aca6500471463951c98ba53fbe582cc196974293
Ana C <apspfc@gmail.com>
2021-11-10 15:35:25 +0000

    styled properly

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ const app = express();
 
 app.use(express.static("public"));
 
-
-app.get("/home", (request, response)=> {
-    response.sendFile(__dirname + "/views/home.html")
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/home.html");
 });
 
-app.get("/works", (request, response)=> {
-    response.sendFile(__dirname + "/views/works.html")
+app.get("/works", (request, response) => {
+  response.sendFile(__dirname + "/views/works.html");
 });
 
-app.get("/about", (request, response)=> {
-    response.sendFile(__dirname + "/views/about.html")
+app.get("/about", (request, response) => {
+  response.sendFile(__dirname + "/views/about.html");
 });
 
-app.get("/bonus", (request, response)=> {
-    response.sendFile(__dirname + "/views/bonus.html")
+app.get("/bonus", (request, response) => {
+  response.sendFile(__dirname + "/views/bonus.html");
 });
 
-app.listen(3000, ()=> console.log("Listening on port 3000."))
+app.listen(3000, () => console.log("Listening on port 3000."));
